ae9b5e74effe1bd71cd0d579d5d5c4bb0e64b616
Ben Hall <rcmast3r1@gmail.com>
2024-02-28 10:19:17 -0500

    first commit

---
+++ hpc_setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd ~
+mkdir -p .local/bin
+echo "export PATH=\$PATH:\$HOME/.local/bin" >> .profile
+curl -L https://hydra.nixos.org/job/nix/master/buildStatic.x86_64-linux/latest/download-by-type/file/binary-dist > .local/bin/.nix-wrapped
+chmod +x .local/bin/.nix-wrapped
